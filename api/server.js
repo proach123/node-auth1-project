bef8463d1956cbe364c299be01697a877d9cc6fc
Patrick Roach <patrickroach@Patricks-MacBook-Air.local>
2020-02-26 16:31:44 -0700

    yeah

--- api/server.js
+++ api/server.js
@@ -1,16 +1,55 @@
 const express = require('express');
+const helmet = require("helmet");
+const cors = require("cors");
+
+const session = require("express-session"); // npm i express-session
+const KnexStore = require("connect-session-knex")(session); 
+
 
 const apiRouter = require('./api-router.js');
 const authRouter = require('../auth/auth-router');
+const restricted = require('../auth/restricted-middleware')
 const userRouter = require('../users/users-router');
 const configureMiddleware = require('./configure-middleware.js');
 
+const knex = require("../database/dbConfig");
+
 const server = express();
+
+const sessionConfig = {
+    name: "super secret cookiemonster",
+    secret: "keep it secret, keep it safe!",
+    resave: false,
+    saveUninitialized: true, // related to GDPR compliance
+    cookie: {
+      maxAge: 1000 * 60 * 10,
+      secure: false, // should be true in production
+      httpOnly: true, // tr ue means JS can't touch the cookie
+    },
+    // remember the new keyword
+    store: new KnexStore({
+      knex,
+      tablename: "sessions",
+      createtable: true,
+      sidfieldname: "sid",
+      clearInterval: 1000 * 60 * 15,
+    }),
+  };
 
 configureMiddleware(server);
 
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig))
+
 server.use('/api', apiRouter);
 server.use('/auth', authRouter)
-server.use('/users', userRouter)
+server.use('/users', restricted, userRouter)
+
+server.get("/", (req, res) => {
+    // console.log(req.session);
+    res.json({ api: "up" });
+  });
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -20,20 +20,41 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  let { username, password } = req.body;
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          req.session.loggedIn = true;
+          req.session.username = user.username;
+  
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+          });
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
+router.get("/logout", (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.status(500).json({
+            you: "can check out any time you like, but you can never leave",
+          });
+        } else {
+          res.status(200).json({ you: "logged out successfully" });
+        }
+      });
+    } else {
+      res.status(200).json({ bye: "felicia" });
+    }
+  });
 
 module.exports = router;
